7e83b821566acb8cf11208703cc6407f1b920b0a
ashishgarg880 <ashishgarg880@gmail.com>
2021-11-22 16:55:37 +0530

    success

--- css/index.js
+++ css/index.js
@@ -1,32 +1,73 @@
-function calc(){
-    var current_date  = document.getElementById('start_day').value;
-    var current_month  = document.getElementById('start_month').value;
-    var current_year  = document.getElementById('start_year').value;
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+function calc()
+{
+    let calculated_date,calculated_month,calculated_year;
+    var current_date  = document.getElementById('end_day').value;
+    var current_month  = document.getElementById('end_month').value;
+    var current_year  = document.getElementById('end_year').value;
 
-    var date_birth = document.getElementById('end_day').value;
-    var month_birth = document.getElementById('end_month').value;
-    var year_birth = document.getElementById('end_year').value;
+    var date_birth = document.getElementById('start_day').value;
+    var month_birth = document.getElementById('start_month').value;
+    var year_birth = document.getElementById('start_year').value;
 
-    var result_div = document.getElementsByTagName('textarea')[0];
-    var div_result1 =  date_birth - current_date;
     var div_result2 =  month_birth - current_month;
-    if(current_year<year_birth)
-    {
-            var div_result3 =  year_birth - current_year;
-    }
-    else{
-        var div_result3 = "year is max";
+    if (
+        year_birth > current_year ||
+        (month_birth > current_month && year_birth == current_year) ||
+        (date_birth > current_date && month_birth == current_month && year_birth == current_year)
+    ) {
+        alert("NOT BORN YET");
+        return;
     }
 
-    var div_result = div_result1+"Days"+div_result2+" Month"+div_result3+"Year";
+    leapChecker(current_year);
+
+    if (
+        year_birth > current_year ||
+        (month_birth > current_month && year_birth == current_year) ||
+        (date_birth > current_date && month_birth == current_month && year_birth == current_year)
+    ) {
+        alert("NOT BORN YET");
+        return;
+    }
+
+
+    calculated_year = current_year - year_birth;
+
+    if(current_month >= month_birth){
+        calculated_month = current_month - month_birth;
+    } else {
+        calculated_year--;
+        calculated_month = 12 + current_month - month_birth 
+    }
     
-    var span = document.createElement('span');
-    
-    span.innerHTML =  div_result;
-    span.className="header-h2";
-    result_div.appendChild(span);
-    console.log(span);
-    console.log(div_result1);
-    console.log(div_result2);
-    console.log(div_result3);
+    if(current_date >= date_birth){
+        calculated_date = current_date - date_birth;
+    } 
+    else {
+        calculated_month--;
+        let days = months[current_month - 2];
+        calculated_date = days + current_date - date_birth;
+        if(calculated_month < 0){
+            calculated_month = 11;
+            calculated_year--;
+        }
+    }
+    displayResult(calculated_date,calculated_month,calculated_year);    
+}
+
+function displayResult(bDate,bMonth,bYear){
+    var result_div = document.getElementsByTagName('textarea')[0];
+    result_div.innerHTML = bYear+"Year,\t"+bMonth+"Month,\t"+bDate+"Date\t"+"\n";
+
+}
+function leapChecker(year) 
+{
+    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) 
+    {
+        months[1] = 29;
+    } 
+    else {
+        months[1] = 28;
+    }
 }
